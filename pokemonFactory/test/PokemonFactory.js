ed3bef1ff347446b7e84db8556e3b9c3e2f10a8f
AngelSanchezT <ajsanchez22@gmail.com>
2023-03-17 10:43:55 -0500

    Reto #4 - Estudiante Super Sayayin Los Pokemons  pueden pertenecer a más de un tipo (Type), por ejemplo: Bulbasaur es de tipo Grass y Poison. Proponga una solución e impleméntela.
    Los Pokemons  tienen debilidades (Weaknesses) las cuales pueden ser otros tipos de pokemones. Por ejemplo,  Bulbasaur es débil contra pokemones de tipo Fire, Flying, Ice, Psychic. Proponga una solución e impleméntela.

--- pokemonFactory/test/PokemonFactory.js
+++ pokemonFactory/test/PokemonFactory.js
@@ -21,8 +21,8 @@ describe("Pokemon contract", function () {
 
         const hardhatPokemon = await PokemonFactory.deploy();
 
-        await hardhatPokemon.createPokemon( "bulbasaur", 1, []);
-        await hardhatPokemon.createPokemon( "ivysaur", 2, []);
+        await hardhatPokemon.createPokemon( "bulbasaur", 1, [], [], []);
+        await hardhatPokemon.createPokemon( "ivysaur", 2, [], [], []);
 
         const pokemons = await hardhatPokemon.getAllPokemons();
 
@@ -35,7 +35,7 @@ describe("Pokemon contract", function () {
         const PokemonFactory = await ethers.getContractFactory("PokemonFactory");
         const hardhatPokemon = await PokemonFactory.deploy();
         
-        await expect( hardhatPokemon.createPokemon( "bulbasaur", 0 , [])).to.be.reverted;
+        await expect( hardhatPokemon.createPokemon( "bulbasaur", 0 , [], [], [])).to.be.reverted;
         
     });
 
@@ -44,8 +44,8 @@ describe("Pokemon contract", function () {
         const PokemonFactory = await ethers.getContractFactory("PokemonFactory");
         const hardhatPokemon = await PokemonFactory.deploy();
         
-        await expect( hardhatPokemon.createPokemon( "", 1 , [])).to.be.reverted;
-        await expect( hardhatPokemon.createPokemon( "a", 1,  [])).to.be.reverted;
+        await expect( hardhatPokemon.createPokemon( "", 1 , [], [], [])).to.be.reverted;
+        await expect( hardhatPokemon.createPokemon( "a", 1,  [], [], [])).to.be.reverted;
         // await expect( hardhatPokemon.createPokemon( undefined, 1, [] )).to.be.reverted;
         // await expect( hardhatPokemon.createPokemon( null, 1, [] )).to.be.reverted;
 
@@ -56,14 +56,14 @@ describe("Pokemon contract", function () {
         const PokemonFactory = await ethers.getContractFactory("PokemonFactory");
         const hardhatPokemon = await PokemonFactory.deploy();
 
-        hardhatPokemon.createHability("stench", "This Pokémon's damaging moves have a 10% chance to make the target", 1);
-        hardhatPokemon.createHability("drizzle", "ummons rain that lasts indefinitely upon entering battle.", 2);
-        hardhatPokemon.createHability("speed-boost", "This Pokémon's Speed rises one stage after each turn.", 3);
+        hardhatPokemon.createAbility("stench", "This Pokémon's damaging moves have a 10% chance to make the target", 1);
+        hardhatPokemon.createAbility("drizzle", "ummons rain that lasts indefinitely upon entering battle.", 2);
+        hardhatPokemon.createAbility("speed-boost", "This Pokémon's Speed rises one stage after each turn.", 3);
 
         const abilities = await hardhatPokemon.getAllAbilities();
 
-        await hardhatPokemon.createPokemon( "bulbasaur", 1, [1,2]);
-        await hardhatPokemon.createPokemon( "ivysaur", 2, [1,2,3]);
+        await hardhatPokemon.createPokemon( "bulbasaur", 1, [1,2], [], []);
+        await hardhatPokemon.createPokemon( "ivysaur", 2, [1,2,3], [], []);
 
         const pokemons = await hardhatPokemon.getAllPokemons();
 
@@ -76,6 +76,58 @@ describe("Pokemon contract", function () {
         const abilitiesByPokemon = await hardhatPokemon.getAbilitiesByPokemonId(1);
         // console.log(abilitiesByPokemon);
         expect(abilitiesByPokemon.length).to.equal(2);
+
+    });
+
+    it("Challenge 4 - Pokemon with Types and Weaknesses", async function () {
+
+        const PokemonFactory = await ethers.getContractFactory("PokemonFactory");
+        const hardhatPokemon = await PokemonFactory.deploy();
+
+        // Abilities
+        hardhatPokemon.createAbility("stench", "This Pokémon's damaging moves have a 10% chance to make the target", 1);
+        hardhatPokemon.createAbility("drizzle", "ummons rain that lasts indefinitely upon entering battle.", 2);
+        hardhatPokemon.createAbility("speed-boost", "This Pokémon's Speed rises one stage after each turn.", 3);
+
+        // Types
+        hardhatPokemon.createType("plant", "plant", 1);
+        hardhatPokemon.createType("poison", "poison", 2);
+        hardhatPokemon.createType("", "", 3);
+
+        const types = await hardhatPokemon.getAllPokemonTypes();
+
+        // Weaknesses
+        hardhatPokemon.createWeakness("fire", "Fire", 1);
+        hardhatPokemon.createWeakness("psychic", "Psychic", 2);
+        hardhatPokemon.createWeakness("flying", "Flying", 3);
+        hardhatPokemon.createWeakness("ice", "Ice", 4);
+
+        const weaknesses = await hardhatPokemon.getAllPokemonWeaknesses();
+
+        // Pokemons
+
+        await hardhatPokemon.createPokemon( "bulbasaur", 1,  [1,2], [1,2], [1,2,3]);
+        await hardhatPokemon.createPokemon( "ivysaur", 2,  [1,2], [1,2], [1,2,3]);
+
+        const pokemons = await hardhatPokemon.getAllPokemons();
+
+        // console.log(types);
+        expect(types.length).to.equal(3);
+
+        // console.log(weaknesses);
+        expect(weaknesses.length).to.equal(4);
+
+        // console.log(pokemons);
+        expect(pokemons.length).to.equal(2);
+
+        const typesByPokemon = await hardhatPokemon.getTypesByPokemonId(1);
+        const weaknessesByPokemon = await hardhatPokemon.getWeaknessesByPokemonId(1);
+
+        // console.log(typesByPokemon);
+        expect(typesByPokemon.length).to.equal(2);
+
+        // console.log(weaknessesByPokemon);
+        expect(weaknessesByPokemon.length).to.equal(3);
 
     });
 
