f8693c7d4badd9a8428c039cf63c73da35627533
AngelSanchezT <ajsanchez22@gmail.com>
2023-04-16 01:43:49 -0500

    Creación metodo visit y las variables de estado del contrato

--- VisitCounter/contracts/VisitCounter.sol
+++ VisitCounter/contracts/VisitCounter.sol
@@ -3,6 +3,22 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract VisitCounter {
-    
-}
+    struct Person {
+        uint256 counter;
+        string[] messages;
+    }
 
+    mapping(address => Person) private people;
+    uint256 totalVisits;
+
+    function visit(string calldata _message) external returns (uint256) {
+        people[msg.sender].counter++;
+        people[msg.sender].messages.push(_message);
+        totalVisits++;
+        return (totalVisits);
+    }
+
+    function getPeople(address _addr) external view returns(Person memory) {
+        return(people[_addr]);
+    }
+}
