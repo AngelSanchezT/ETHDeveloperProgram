c5c13b3a5cda9752ad38dc47e35c35a05364424e
AngelSanchezT <ajsanchez22@gmail.com>
2023-04-16 02:09:43 -0500

    Creación de Test Unitarios para el metodo Visitar

--- VisitCounter/test/VisitCounter.test.js
+++ VisitCounter/test/VisitCounter.test.js
@@ -3,10 +3,15 @@ const { ethers } = require("hardhat");
 
 describe("VisitCounter", function () {
   let visitCounter;
+  let owner;
 
   this.beforeEach(async () => {
     const VisitCounter = await ethers.getContractFactory("VisitCounter");
     visitCounter = await VisitCounter.deploy();
+
+    [owner] = await ethers.getSigners();
+
+
   });
 
   it("Registrar una visita", async function () {
@@ -14,11 +19,15 @@ describe("VisitCounter", function () {
     const numVisits = await visitCounter.getTotalVisits();
     expect(numVisits).to.equal(1);
   });
-  
+
   it("Registrar dos visitas", async function () {
     await visitCounter.visit("Hello, First Message");
     await visitCounter.visit("Hello, Seconds Message");
     const numVisits = await visitCounter.getTotalVisits();
+    const people = await visitCounter.getPeople(owner.address);
+
     expect(numVisits).to.equal(2);
+    expect(people.messages.length).to.equal(2);
+    
   });
 });
