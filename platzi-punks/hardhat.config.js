52a13798f367373d7d3859c51c292f2512b8a0a0
AngelSanchezT <ajsanchez22@gmail.com>
2023-05-22 01:50:02 -0500

    Enviroment Config

--- platzi-punks/hardhat.config.js
+++ platzi-punks/hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+    },
+  },
+
 };
